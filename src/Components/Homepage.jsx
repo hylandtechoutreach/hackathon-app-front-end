512af77c46a961eb83898093726b8ae48d611cf9
Halina <trileanzah@gmail.com>
2021-08-05 13:55:51 -0400

    Added jokes and css on homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,9 +1,68 @@
 import "../css/Homepage.css";
+import React, { Component } from "react";
+import QuoteAndAuthor from "./QuoteAndAuthor";
+import quotes from "./QuoteDB";
 
-function Homepage() {
-    return (
-        <h2> HOME</h2>
-    )
-}
+export default class App extends Component {
+  //state
+  state = {
+    quote: quotes[0].quote,
+    author: quotes[0].author,
+  };
 
-export default Homepage;
+  //generate diffrent quote function
+  generateRandomQuote = (arr) => {
+    //get random numbers
+    let num = Math.floor(Math.random() * quotes.length);
+
+    let newQuote = quotes[num];
+
+    //update state
+    this.setState({
+      quote: newQuote.quote,
+      author: newQuote.author,
+    });
+
+    this.shuffleQuotes(quotes);
+  };
+
+  //shuufle quotes function
+  shuffleQuotes = (arr) => {
+    return arr.sort(function () {
+      return 0.5 - Math.random();
+    });
+  };
+
+    render() {
+        return (
+            <div className="homepage">
+              <h1 id="welcome">Welcome To Hyland Hackathon!</h1>
+                 <div className="container">
+                    <div className="left_container">
+                      <div className="Announcements">
+                       <h1> Announce</h1>
+                     </div>
+                      <div className="quotebox">
+              
+                             <h2 className="text-center">Jokes</h2>
+              <div className="quote">
+                <QuoteAndAuthor
+                  generateRandomQuote={this.generateRandomQuote}
+                  quote={this.state}
+                />
+              </div>
+            </div>
+            </div>
+            <div className="right_container">
+            <div className="Itinerary">
+                <h1>things</h1>
+            </div>
+            </div>
+            </div>
+            </div>
+        );
+    };
+};
+
+
+//export default Homepage;
---
+++ src/Components/QuoteDB.js
@@ -0,0 +1,28 @@
+const quotes = [
+    {
+        "quote": "Why did the programmer quit his job?",
+        "author": "because he didnt get arrays"
+    },
+    {
+        "quote": "0 is false and 1 is true, right?",
+        "author": "1"
+    },
+    {
+        "quote": "Why do Java programmers have to wear glasses?",
+        "author": "Because they don't C#"
+    },
+    {
+        "quote": "99 little bugs in the code, 99 bugs in the code, 1 bug fixed... compile again,",
+        "author": "100 little bugs in the code."
+    },
+    {
+        "quote": "An optimist says:'the glass is half-full' A pessimist says:'the glass is half-empty'",
+        "author": "A programmer says:'the glass is twice as large as necessary'"
+    },
+    {
+        "quote": "Hide and seek champion....",
+        "author": "; since 1958"
+    },
+]
+
+export default quotes;
