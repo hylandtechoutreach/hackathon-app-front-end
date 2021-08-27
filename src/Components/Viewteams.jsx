f48fbca2f53f567d2e4be8b3d60260b2c7f0d256
Joseph Maxwell <joseph.maxwell@hyland.com>
2021-08-27 12:58:56 -0400

    update "view teams" to pull team submission data

--- src/Components/Viewteams.jsx
+++ src/Components/Viewteams.jsx
@@ -1,9 +1,43 @@
 import "../css/Viewteams.css";
 
-function Viewteams() {
-    return (
-        <h2> What are the teams working on? </h2>
-    )
-}
+import React,{Component} from 'react';
 
-export default Viewteams;
+export default class teamSubmission extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={deps:[]}
+    }
+
+    refreshList(){
+        fetch( 'http://127.0.0.1:8000/teamSubmission')
+        .then(response=>response.json())
+       
+        .then(data=>{
+            this.setState({deps:data});
+        });
+    }
+    componentDidMount(){
+        this.refreshList();
+    }
+    componentDidUpdate(){
+        this.refreshList();
+    }
+    render(){
+        const {deps}=this.state;
+        return(
+            <div>
+                <h2>Team Submissions</h2>
+                {deps.map(dep=>
+                    <div>
+                    <h4>Team Submission {dep.TeamId} </h4>
+                    <p>Video link: {dep.VideoLink}</p>
+                    <p>GitHub link: {dep.GithubLink}</p>
+                    <p>Project idea: {dep.ProjectIdea}</p>
+                    <p>Coding languages: {dep.CodingLanguages}</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
