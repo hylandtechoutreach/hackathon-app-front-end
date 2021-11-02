d02e05a8f0b14062974aa79d27d999914f926e00
T-L-H <47262063+T-L-H@users.noreply.github.com>
2021-11-02 17:40:10 -0400

    Login Frontend (#8)
    * Added submission react
    
    * Login Frontend
    
    * Fixed the book problem.

--- src/Components/Otherteams.jsx
+++ src/Components/Otherteams.jsx
@@ -1,6 +1,11 @@
 import "../css/Otherteams.css";
 
 function Otherteams() {
+    var test = localStorage.getItem("LOGGEDIN");
+      
+    if(test !== "True"){
+      window.location.href = '/Login App';
+    }
     return (
         <h2> NOT YOUR TEAM</h2>
     )
---
+++ src/Components/test.js
@@ -0,0 +1,10 @@
+
+
+function TEST() {
+    localStorage.setItem("LOGGEDIN", "True");
+    return (
+        <meta http-equiv="refresh" content="0;URL='/Homepage'" />    
+    )
+}
+
+export default TEST;
